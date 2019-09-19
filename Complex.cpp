ed0ba86f273b9e842759f99a225f8b7f57baeb53
Kevin Henry <kev7blu@gmail.com>
2019-09-18 22:33:01 -0600

    commit 1.0

--- Complex.cpp
+++ Complex.cpp
@@ -7,3 +7,150 @@ Complex::Complex()
 }
 
 
+// Constructors
+Complex::Complex(double re, double im)
+{
+	_real = re;
+	_imag = im;
+}
+
+Complex::Complex(const Complex& z)
+{
+	_real = z.real();
+	_imag = z.imag();
+}
+
+// access values
+double Complex::real() const
+{
+	return _real;
+}
+
+double Complex::imag() const 
+{
+	return _imag;
+}
+
+
+// basic math operations
+Complex operator+(const Complex& a, const Complex& b)
+{
+	double reals = a.real() + b.real();
+	double imags = a.imag() + b.imag();
+
+	Complex total(reals,imags);
+
+	return total;
+}
+
+Complex operator-(const Complex& a, const Complex& b)
+{
+	double reals = a.real() - b.real();
+        double imags = a.imag() - b.imag();
+
+	Complex total(reals,imags);
+
+	return total;
+}
+
+Complex operator*(const Complex& a, const Complex& b)
+{
+	double first = a.real() * b.real();
+        double outside = a.real() * b.imag(); 
+	double inside = b.real() * a.imag();
+	double last  = -1 * a.imag() * b.imag();
+
+	double imags = outside + inside;
+	double reals = first + last;
+
+	Complex total(reals,imags);
+
+	return total;
+}
+/*
+Complex operator/(const Complex& a, const Complex& b);
+{
+	
+        
+//	VERY BUGGY
+
+
+	Complex total(reals,imags);
+
+	return total;
+}
+*/
+// norm returns the squared magnitude of z
+double norm(const Complex& z)			//FOIL
+{						//(8+i)(8+i)
+	double first = z.real() * z.real();	//8*4
+	double inNout = 2*z.real() * z.imag();	//(8*i + 8*i) = 2(8*i)
+	double last = -1*z.imag() * z.imag();	//i*i = -1 
+
+	double reals = first + last;		//inNout = inner + outer parts of FOIL
+	double total = reals + inNout;
+
+	return total;
+}
+
+
+// conj returns the complex conjugate of z
+Complex conj(const Complex& z)
+{							//(a + bi)(a - bi)
+	double reals1 = z.real() * z.real();		//a * a = a^2
+        double reals2 = -1*z.imag() * z.imag();		// bi * bi = b^2 i^2 => i^2 = -1 => -1*b^2
+
+	double reals_tot = reals1 + reals2;		//no need to calculate inner and outer since they cancel each other out
+	
+	Complex total(reals_tot, 0);
+
+	return total;
+}
+
+
+// comparison
+bool operator==(const Complex& a, const Complex& b)
+{
+	if ((a.real() == b.real()) && (a.imag() == b.imag()))
+		return true;
+	else
+		return false;	
+
+}
+
+bool operator==(const Complex& a, double r)
+{
+	if (a.real() + a.imag() == r)
+		return true;
+	else
+		return false;
+
+}
+
+bool operator!=(const Complex& a, const Complex& b)
+{
+	if ((a.real() != b.real()) && (a.imag() != b.imag()))
+		return true;
+        else
+		return false;
+
+
+}
+
+bool operator!=(const Complex& a, double r)
+{	
+	if (a.real() + a.imag() != r)
+		return true;
+	else
+		return false;
+}
+
+// write the complex number z to the output stream in the
+// format "6+5i" or "6-5i"
+
+std::ostream& operator<<(std::ostream& out, const Complex& z)
+{
+	out<< z.real() << (z.imag() < 0? "-": "+") << z.imag();
+
+	return out;
+}
